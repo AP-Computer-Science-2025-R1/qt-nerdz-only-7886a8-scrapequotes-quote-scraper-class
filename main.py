1cdfa9ad39dd850aa5f54b41a41123574a8b220e
Akib2026 <akiba4@nycstudents.net>
2025-10-25 01:45:00 +0000

    final

--- main.py
+++ main.py
@@ -5,17 +5,17 @@
 # Members: Arshia, Pearl, Alvin, David, Svitozar (Ed), Ronny, Akib
 # ==================================
 def group_introductions():
-print ("Group members and their roles")
-print(" ")
-print("Akib - group_introductions")
-print("Alvin - scrape_all_quotes")
-print("Pearl - scrape_all_quotes")
-print("Svitozar - save_quotes_to_disk")
-print("David - load_quotes_from_disk")
-print("Ronny - get_random_quote")
-print(" ")
-date = input("Please enter today's date: ")
-print("successful") 
+    print("Group members and their roles")
+    print(" ")
+    print("Akib - group_introductions")
+    print("Alvin - scrape_all_quotes")
+    print("Pearl - scrape_all_quotes")
+    print("Svitozar - save_quotes_to_disk")
+    print("David - load_quotes_from_disk")
+    print("Ronny - get_random_quote")
+    print(" ")
+    date = input("Please enter today's date: ")
+    print("successful") 
 
 single_quote = {
   'text': 'The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.',
